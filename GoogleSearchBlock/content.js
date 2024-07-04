cc9514a4bd371fe5a7feeb044c0ccfc4a8af3ea3
UnderKo <UnderKo@DESKTOP-ATCVI05>
2024-07-04 15:50:33 +0300

    asd

--- GoogleSearchBlock/content.js
+++ GoogleSearchBlock/content.js
@@ -1,29 +1,18 @@
-// function EvMutation(mutation) {
-//   console.log(mutation);
-// }
-
-// if (document.URL.startsWith("https://www.google.com/")) {
-//   var search_element = document.getElementById("search");
-//   if (search_element) {
-//     var mutationObserver = new MutationObserver(function (mutations) {
-//       mutations.forEach(EvMutation);
-//     });
-//     mutationObserver.observe(search_element, {
-//       attributes: true,
-//       characterData: true,
-//       childList: true,
-//       subtree: true,
-//       attributeOldValue: true,
-//       characterDataOldValue: true,
-//     });
-//   }
-// }
 function isDiv(nodeName) {
   return nodeName == "DIV";
 }
-function find_div_domens(){
+var block_ = [
+  "https://education.yandex.ru",
+  "https://znanierussia.ru",
+  "https://blog.skillfactory.ru",
+  "https://skillfactory.ru",
+  "https://skillbox.ru",
+  "https://otus.ru",
+  "https://www.sravni.ru"
+];
+var search_element = document.getElementById("search");
+function find_div_domens() {
   var list = [];
-  var search_element = document.getElementById("search");
   search_element.childNodes.forEach((element) => {
     if (isDiv(element.nodeName)) {
       element.childNodes.forEach((element2) => {
@@ -31,7 +20,7 @@ function find_div_domens(){
           element2.childNodes.forEach((element3) => {
             if (isDiv(element3.nodeName)) {
               if (element3.className != "") {
-                console.log(element3);
+                
                 list.push(element3);
               } else {
                 element3.childNodes.forEach((div__) => {
@@ -48,7 +37,52 @@ function find_div_domens(){
   });
   return list;
 }
-find_div_domens().forEach((div)=>{
-  console.log(div)
-})
 
+function valid(chi, i, action) {
+  if (chi) {
+    var chll_ = chi.childNodes;
+    if (chll_.length > 0) {
+      action(chll_[i]);
+    }
+  }
+}
+
+function isblock(link) {
+  for (let index = 0; index < block_.length; index++) {
+    if (link.startsWith(block_[index])) return true;
+  }
+
+  return false;
+}
+
+find_div_domens().forEach((div) => {
+  valid(div, 0, (el) => {
+    valid(el, 0, (el2) => {
+      valid(el2, 0, (el3) => {
+        valid(el3, 0, (el4) => {
+          valid(el4, 0, (el5) => {
+            valid(el5, 0, (el6) => {
+              valid(el6, 0, (el7) => {
+                if (el7.href != null) {
+                  if (isblock(el7.href)) {
+                    console.log(el7.href);
+                    div.style.display = "none";
+                  }
+                } else {
+                  valid(el7, 0, (el8) => {
+                    if (el7.href != null) {
+                      if (isblock(el8.href)) {
+                        console.log(el8.href);
+                        div.style.display = "none";
+                      }
+                    }
+                  });
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
